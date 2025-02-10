a3c2363e79bf1f0e1486bf0f6ee4e795fc39e104
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-10 07:08:21 +0100

    authService.js
    Updated backend url

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import {toast} from 'react-toastify'
 
-const API_URL = '/api/users/'
+const API_BASE_URL = process.env.REACT_APP_API_URL
+const API_URL = 'api/users/'
  
 //Register User
 const registeruser = async(userData) =>{
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(`${API_BASE_URL}/API_URL`, userData)
   if(response.status===201){
     localStorage.setItem('user', JSON.stringify(response.data))
     setTimeout(()=>{
@@ -19,7 +20,7 @@ const registeruser = async(userData) =>{
 // Login User
 const loginuser = async(loginData)=>{
   try{
-    const response = await axios.post(API_URL + 'login', loginData ) 
+    const response = await axios.post(`${API_BASE_URL}/API_URL + 'login', loginData ) 
     if(response.status===201){
       localStorage.setItem('user', JSON.stringify(response.data))
       setTimeout(()=>{
