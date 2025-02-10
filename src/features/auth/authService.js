0ad2e08f9d04a6d74338e4bbaaa0caed7ff8edd1
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-10 07:15:54 +0100

    authService.js
    update

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {toast} from 'react-toastify'
 
-const API_BASE_URL = process.env.REACT_APP_API_URL
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 const API_URL = 'api/users/'
  
 //Register User
